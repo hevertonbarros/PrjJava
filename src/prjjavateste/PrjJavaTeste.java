b18081056850d6d27baf223f3dbdb2e9d44cfa2f
Ton <hevertonbarros@gmail.com>
2019-02-20 20:18:56 -0300

    2 Laços for

--- src/prjjavateste/PrjJavaTeste.java
+++ src/prjjavateste/PrjJavaTeste.java
@@ -24,7 +24,9 @@ public class PrjJavaTeste {
             System.out.println("["+ i + "]");
         }
         
-        
+        for (int i=0; i<5; i++){
+            System.out.println("["+ i + "]");
+        }
         
     }
     
