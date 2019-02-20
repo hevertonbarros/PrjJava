0dd4a5ffd7601fe03a7aa1ce77a05ecd77139643
Ton <hevertonbarros@gmail.com>
2019-02-20 20:07:52 -0300

    Laço for

--- src/prjjavateste/PrjJavaTeste.java
+++ src/prjjavateste/PrjJavaTeste.java
@@ -16,9 +16,9 @@ public class PrjJavaTeste {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        for (int i=0; i<10; i++)
+        for (int i=0; i<10; i++){
             System.out.println("["+ i + "]");
-        
+        }
     }
     
 }
