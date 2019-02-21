f77c0e6f6753bcba8e6ec667f53dbf34932c2236
Ton <hevertonbarros@gmail.com>
2019-02-20 21:18:38 -0300

    Voltando

--- src/prjjavateste/PrjJavaTeste.java
+++ src/prjjavateste/PrjJavaTeste.java
@@ -16,18 +16,7 @@ public class PrjJavaTeste {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        for (int i=0; i<10; i++){
-            System.out.println("["+ i + "]");
-        }
-        
-        for (int i=10; i>0; i--){
-            System.out.println("["+ i + "]");
-        }
-        
-        
-        for (int i=0; i<5; i++){
-            System.out.println("["+ i + "]");
-        }
+        System.out.println("Fim.");
         
     }
     
