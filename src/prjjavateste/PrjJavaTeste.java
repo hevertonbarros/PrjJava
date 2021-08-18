d8c18b57be16950d22200b8ec606b6a2af2d9254
Ton <hevertonbarros@gmail.com>
2021-08-18 18:42:18 -0300

    Envio de teste

--- src/prjjavateste/PrjJavaTeste.java
+++ src/prjjavateste/PrjJavaTeste.java
@@ -16,7 +16,7 @@ public class PrjJavaTeste {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Fim.");
+        System.out.println("Teste de Fim.");
         
     }
     
